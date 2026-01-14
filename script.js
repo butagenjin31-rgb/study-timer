69cb304faa6bea6ca243a740b89942030a8d5386
butagenjin31-rgb <butagenjin31@gmail.com>
2026-01-14 22:18:00 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-const timerDisplay=document.getElementById("timer");
-const startButton=document.getElementById("start");
-let remainingseconds=25*60;
-let intervalId=null;
+const timerDisplay = document.getElementById("timer");
+const startButton = document.getElementById("start");
 
-function updateTimer(){
-  const minutes=Math.floor(remainingSeconds/60)
-  const seconds=remainingSeconds %60;
-  timerDisplay.textContent=
-    String(minutes).padStart(2,"0")+":"+
-    String(seconds).padStart(2,"0");
+let remainingSeconds = 25 * 60;
+let intervalId = null;
+
+function updateTimer() {
+  const minutes = Math.floor(remainingSeconds / 60);
+  const seconds = remainingSeconds % 60;
+  timerDisplay.textContent =
+    String(minutes).padStart(2, "0") + ":" +
+    String(seconds).padStart(2, "0");
 }
 
-startButton.addEventListener("click",()=>{
-  //すでに動いていたら二重起動しない
-if(intervalId !==null)return;
+startButton.addEventListener("click", () => {
+  // すでに動いていたら二重起動しない
+  if (intervalId !== null) return;
 
-  interbalId=setInterval(() =>{
-    remainingSecond--;
+  intervalId = setInterval(() => {
+    remainingSeconds--;
     updateTimer();
 
-                      if(remainingSeconds <=0){
-                        clearInterval(intervalId);
-                        intervalId = null;
-                        remainingSeconds = 0;
-                        updateTimer();
-                      }
-  },1000);
+    if (remainingSeconds <= 0) {
+      clearInterval(intervalId);
+      intervalId = null;
+      remainingSeconds = 0;
+      updateTimer();
+    }
+  }, 1000);
 });
 
-//最初の表示を整える
+// 最初の表示を整える
 updateTimer();
-    
