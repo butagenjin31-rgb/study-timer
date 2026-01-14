df71ff6ed996c6bdfe969aea9f1271ce7d4633a1
butagenjin31-rgb <butagenjin31@gmail.com>
2026-01-14 22:05:25 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 const timerDisplay=document.getElementById("timer");
 const startButton=document.getElementById("start");
-timerDisplay.textContent="25:00";
+let remainingseconds=25*60;
+let intervalId=null;
+
+function updateTimer(){
+  const minutes=Math.floor(remainingSeconds/60)
+  const seconds=remainingSeconds %60;
+  timerDisplay.textContent=
+    String(minutes).padStart(2,"0")+":"+
+    String(seconds).padStart(2,"0");
+}
+
+startButton.addEventListener("click",()=>{
+  //すでに動いていたら二重起動しない
+if(intervalId !==null)return;
+
+  interbalId=setInterval(() =>{
+    remainingSecond--;
+    updateTimer();
+
+                      if(remainingSeconds <=0){
+                        clearInterval(intervalId);
+                        intervalId = null;
+                        remainingSeconds = 0;
+                        updateTimer();
+                      }
+  },1000);
+});
+
+//最初の表示を整える
+updateTimer();
+    
